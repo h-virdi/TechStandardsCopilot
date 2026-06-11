a3892ae721db89e6dd1c48495930eff05c8fb128
h-virdi <harnoorvirdi89@gmail.com>
2026-06-11 08:22:23 +0800

    Add protoype chatbot

---
+++ debugging.py
@@ -0,0 +1,2 @@
+with open(".env", "r") as f:
+    print(repr(f.read()))
--- ingest.py
+++ ingest.py
@@ -1,11 +1,55 @@
-from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
+import os
 
-# Loading pdfs
-reader = PdfReader("CCS_Cybersec_guidelines.pdf")
-text = ""
-for page in reader.pages:
-  text += page.extract_text()
-print(text[:1000])
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+from utils import load_pdf, split_text
+
+load_dotenv()
+
+PDF_FOLDER = "standards"
+DB_FOLDER = "vector_db"
+
+all_chunks = []
+all_metadatas = []
+
+print("Loading PDFs...")
+
+for filename in os.listdir(PDF_FOLDER):
+
+    if not filename.endswith(".pdf"):
+        continue
+
+    filepath = os.path.join(PDF_FOLDER, filename)
+
+    print(f"Processing {filename}")
+
+    text = load_pdf(filepath)
+
+    chunks = split_text(text)
+
+    for i, chunk in enumerate(chunks):
+
+        all_chunks.append(chunk)
+
+        all_metadatas.append(
+            {
+                "source": filename,
+                "chunk": i
+            }
+        )
+
+print(f"Total chunks: {len(all_chunks)}")
+
+embeddings = OpenAIEmbeddings()
+
+db = Chroma.from_texts(
+    texts=all_chunks,
+    embedding=embeddings,
+    metadatas=all_metadatas,
+    persist_directory=DB_FOLDER
+)
+
+print("Vector database created.")
